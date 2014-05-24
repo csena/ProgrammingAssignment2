3e96914958ac4d2b584e061890e6be67bd9f7bd4
Carlos Sena <cvgsena@gmail.com>
2014-05-24 13:03:11 -0300

    Fixed line break problems in markdown

--- README.md
+++ README.md
@@ -14,11 +14,17 @@ m     the "special" matrix object created and returned by makeCacheMatrix
 
 ### Examples (taken from the Examples of ?solve)
 hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+
 h8 <- hilbert(8)
+
 m1 <- makeCacheMatrix(h8)
+
 inv <- cacheSolve(m1)
 
 inv   # inverse of h8
+
 cacheSolve(m1) # will print "getting cached data"
+
 m1$set(inv)
+
 inv   # inverse of the inverse, equals to h8
--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,4 @@
 ## makeCacheMatrix and cacheSolve allows the caching of the inverse of a square matrix
-### Usage:
-# makeCacheMatrix(x)
-# cacheSolve(m)
-### Arguments
-# x     a square matrix
-# m     the "special" matrix object created and returned by makeCacheMatrix
-### Examples (taken from the Examples of ?solve)
-# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
-# h8 <- hilbert(8)
-# m1 <- makeCacheMatrix(h8)
-# inv <- cacheSolve(m1)
-# inv   # inverse of h8
-# cacheSolve(m1) # will print "getting cached data"
-# m1$set(inv)
-# inv   # inverse of the inverse, equals to h8
-
 
 ## Creates a special matrix object that can cache its inverse matrix
 
